5d5d85b4e098c3cda395a2e13e32d632fb44f3bf
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-03-12 22:23:39 -0500

    finished muse part

--- museapp.js
+++ museapp.js
@@ -23,56 +23,69 @@ server.on("message", function(msg, rinfo) {
 
     batch.push(oscObj);
     // socket.emit('oscdata', oscObj);
-    console.log(oscObj);
+//    console.log(oscObj);
 
+    // Code for concentration
+    if (oscObj.address == "/muse/elements/experimental/concentration") {
+        console.log("concentration: ");
+        console.log(oscObj.args);   
+    }
+//    
+//    if (oscObj.address = "/muse/elements/experimental/mellow") {
+//        console.log("mellow: ");
+//        console.log(oscObj.args);   
+//    }
+      
+      
+      
 
     //START ALPHA PROCESSING CODE
-    // var total = 0;
-    // var active = 0;
-    // var avgAlpha = 0;
-    // if(oscObj.address == "/muse/dsp/elements/alpha"){
-    //   oscObj.args.forEach(function(datum){
-    //     if(datum.value){
-    //       total = total + datum.value;
-    //       active = active + 1;
-    //     }
-    //     avgAlpha = total/active;
-    //   });
-    //   console.log("alpha: "+avgAlpha);
-    //   if(avgAlpha > 0.32){
-    //     calm = calm + 1;
-    //     if(calm == 6){
-    //       socket.emit('threefour', 1);
-    //     }
-    //   }else{
-    //     calm = 0;
-    //   }
-    // }
+//     var total = 0;
+//     var active = 0;
+//     var avgAlpha = 0;
+//     if(oscObj.address == "/muse/dsp/elements/alpha"){
+//       oscObj.args.forEach(function(datum){
+//         if(datum.value){
+//           total = total + datum.value;
+//           active = active + 1;
+//         }
+//         avgAlpha = total/active;
+//       });
+//       console.log("alpha: "+avgAlpha);
+//       if(avgAlpha > 0.32){
+//         calm = calm + 1;
+//         if(calm == 6){
+//           socket.emit('threefour', 1);
+//         }
+//       }else{
+//         calm = 0;
+//       }
+//     }
     //END ALPHA PROCESSING CODE
     //START JAWCLENCH PROCESSING
-    // if(oscObj.address == "/muse/dsp/elements/jaw_clench"){
-    //   console.log(oscObj.args[0].value);
-    //   if(oscObj.args[0].value == 1){
-    //     if((Date.now() - lock) > 4000 ){
-    //       hold = 0;
-    //       socket.emit('threefour', 0);
-    //       lock = Date.now();
-    //     }else{
-    //       hold = hold+1;
-    //       if(hold == 10){
-    //         hold = 0;
-    //         socket.emit('threefour', 1);
-    //         lock = Date.now();
-    //       }
-    //     }
-    //   }
-    // }
+//     if(oscObj.address == "/muse/dsp/elements/jaw_clench"){
+//       console.log(oscObj.args[0].value);
+//       if(oscObj.args[0].value == 1){
+//         if((Date.now() - lock) > 4000 ){
+//           hold = 0;
+//           socket.emit('threefour', 0);
+//           lock = Date.now();
+//         }else{
+//           hold = hold+1;
+//           if(hold == 10){
+//             hold = 0;
+//             socket.emit('threefour', 1);
+//             lock = Date.now();
+//           }
+//         }
+//       }
+//     }
     //END JAWCLENCH
 
     message_counter += 1;
 
 		if(message_counter % 2000 == 0) {
-      console.log('Recieved 2k messages ' + message_counter / 2000);
+//      console.log('Recieved 2k messages ' + message_counter / 2000);
       socket.emit('oscbatch', batch);
       // console.log(batch);
       batch = [];
@@ -87,8 +100,8 @@ server.on("message", function(msg, rinfo) {
 
 server.on("listening", function () {
   var address = server.address();
-  console.log("server listening " +
-      address.address + ":" + address.port);
+//  console.log("server listening " +
+//      address.address + ":" + address.port);
 });
 
 
